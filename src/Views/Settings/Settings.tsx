bb17527cf402f3360251c15509b696b080f3402e
tsundere <benlmartin.work@gmail.com>
2021-11-01 17:30:39 +0000

    Added base features

---
+++ src/Views/Settings/Settings.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Settings = (props: Props) => {
+    return (
+        <div>
+            <h1>This is the settings page!</h1>
+        </div>
+    )
+}
+
+export default Settings
